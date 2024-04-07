587246a643c0d2e34625152588be79ea664aa556
Davide Ferrari <xthehacker2000x@gmail.com>
2024-04-07 14:51:15 +0000

    Fix unused variables in tsconfig.json and remove useEffect import in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react'
+import { useRef, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
