8f0914461fdca909b45c1dbefd1fe9e548611fed
Davide Ferrari <xthehacker2000x@gmail.com>
2024-04-29 22:40:31 +0000

    test1

--- src/services/backendService.tsx
+++ src/services/backendService.tsx
@@ -96,7 +96,7 @@ class BackendService {
     public async loginWithGoogle() {
         await this.client.auth.signInWithOAuth({
             provider: 'google',
-            options: {redirectTo: 'https://ferraridavide.github.io/unipv-aule'}
+            // options: {redirectTo: 'https://ferraridavide.github.io/unipv-aule'}
           })
     }
 
