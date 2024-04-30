de515578fe65c2582257f00206ff9ef1083e9e24
Davide Ferrari <xthehacker2000x@gmail.com>
2024-04-30 16:11:38 +0000

    Contact page

--- src/pages/Contacts.tsx
+++ src/pages/Contacts.tsx
@@ -4,12 +4,10 @@ import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle }
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
-import { MailQuestionIcon, SendIcon } from "lucide-react";
+import { AtSignIcon, MailQuestionIcon, MessageSquareDotIcon, MessageSquareTextIcon, SendIcon } from "lucide-react";
 import { useState } from "react";
 
 function Contacts() {
-  const [subject, setSubject] = useState('');
-  const [description, setDescription] = useState('');
   
     const mailMessage = `Con la presente desideriamo richiedere accesso al calendario relativo all'aula [inserire qui], al fine di integrare le informazioni sul sito https://ferraridavide.github.io/unipv-aule/.%0D%0AQuesta integrazione consentirebbe una più efficace individuazione delle aule disponibili per lo studio.%0D%0ARimaniamo a disposizione per ulteriori informazioni o chiarimenti.%0D%0ACordiali saluti`
 
@@ -28,29 +26,12 @@ function Contacts() {
           </Alert>
           <Card className="mt-4">
             <CardHeader>
-        <CardTitle>Segnala un problema</CardTitle>
-        <CardDescription>
-          In quale area stai riscontrando problemi?
-        </CardDescription>
+        <CardTitle>Contatti</CardTitle>
             </CardHeader>
-            <CardContent className="grid gap-6">
-        <div className="grid gap-2">
-          <Label htmlFor="subject">Oggetto</Label>
-          <Input id="subject" placeholder="Ho bisogno di aiuto con..." value={subject} onChange={e => setSubject(e.target.value)} />
-        </div>
-        <div className="grid gap-2">
-          <Label htmlFor="description">Descrizione</Label>
-          <Textarea
-            id="description"
-            placeholder="Includi tutte le informazioni rilevanti al tuo problema."
-            value={description}
-                onChange={e => setDescription(e.target.value)}
-          />
-        </div>
-            </CardContent>
-            <CardFooter className="justify-end space-x-2 items-right">
-        <Button asChild><a href={`mailto:davide.ferrari05@universitadipavia.it?subject=${subject}&body=${description}`}><SendIcon className="mr-2 h-4 w-4" />Componi</a></Button>
-            </CardFooter>
+            <CardContent className="grid gap-2">
+        <Button asChild><a href={`mailto:davide.ferrari05@universitadipavia.it`}><AtSignIcon className="mr-2 h-4 w-4" />Email</a></Button>
+        <Button asChild><a href={`https://t.me/ferraridavide`}><MessageSquareTextIcon className="mr-2 h-4 w-4" />Telegram</a></Button>
+        </CardContent>
           </Card>
         </>
           );
